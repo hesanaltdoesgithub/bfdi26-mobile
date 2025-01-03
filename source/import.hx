60a19bc2fbd5a4efe1cf1cc9e80986f5ed09e27b
hesanaltdoesgithub <141635145+hesanaltdoesgithub@users.noreply.github.com>
2025-01-03 13:39:47 -0600

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,63 +1,40 @@
 #if !macro
-//Discord API
-#if DISCORD_ALLOWED
-import backend.Discord;
-#end
-
-//Psych
-#if LUA_ALLOWED
-import llua.*;
-import llua.Lua;
-#end
-
-#if ACHIEVEMENTS_ALLOWED
-import backend.Achievements;
-#end
-
-#if sys
-import sys.*;
-import sys.io.*;
-#elseif js
-import js.html.*;
-#end
-
-import backend.Paths;
-import backend.Controls;
-import backend.CoolUtil;
-import backend.MusicBeatState;
-import backend.MusicBeatSubstate;
-import backend.CustomFadeTransition;
-import backend.ClientPrefs;
-import backend.Conductor;
-import backend.BaseStage;
-import backend.Difficulty;
-import backend.Mods;
-
-import objects.Alphabet;
-import objects.BGSprite;
-
-import states.PlayState;
-import states.LoadingState;
-
-#if flxanimate
-import flxanimate.*;
-#end
-
-//Flixel
-import flixel.sound.FlxSound;
+// flixel
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
 import flixel.math.FlxMath;
-import flixel.math.FlxPoint;
-import flixel.util.FlxColor;
-import flixel.util.FlxTimer;
 import flixel.text.FlxText;
-import flixel.tweens.FlxEase;
+import flixel.util.FlxColor;
 import flixel.tweens.FlxTween;
-import flixel.group.FlxSpriteGroup;
-import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.tweens.FlxEase;
+import flixel.util.FlxTimer;
+import flixel.FlxBasic;
+import flixel.math.FlxPoint;
+import flixel.system.FlxSound;
+#if sys
+import sys.io.*;
+import sys.*;
+#end
+#if DISCORD_ALLOWED
+import funkin.api.Discord;
+import funkin.api.Discord.DiscordClient;
+#end
+#if VIDEOS_ALLOWED
+import hxvlc.flixel.*;
+#end
+import Init;
+#if mobile
+import mobile.StorageUtil;
+#end
+import funkin.Paths;
+import funkin.data.ClientPrefs;
+import funkin.data.Conductor;
+import funkin.utils.CoolUtil;
+import funkin.data.Highscore;
+import funkin.states.*;
+import funkin.objects.BGSprite;
+import funkin.backend.MusicBeatState;
 
 using StringTools;
-using backend.FlxObjectTools;
 #end
