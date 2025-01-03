e48a081412a1f88a8db8ff85afcebfb59712adcf
hesanaltdoesgithub <141635145+hesanaltdoesgithub@users.noreply.github.com>
2025-01-03 13:54:29 -0600

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,40 +1,94 @@
 #if !macro
-// flixel
+//Discord API
+#if DISCORD_ALLOWED
+import backend.Discord;
+#end
+
+//Psych
+#if LUA_ALLOWED
+import llua.*;
+import llua.Lua;
+#end
+
+#if ACHIEVEMENTS_ALLOWED
+import backend.Achievements;
+#end
+
+// Mobile Controls
+import mobile.objects.MobileControls;
+import mobile.objects.IMobileControls;
+import mobile.objects.Hitbox;
+import mobile.objects.TouchPad;
+import mobile.objects.TouchButton;
+import mobile.input.MobileInputID;
+import mobile.backend.MobileData;
+import mobile.input.MobileInputManager;
+import mobile.backend.TouchUtil;
+
+// Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
+#if sys
+import sys.*;
+import sys.io.*;
+#elseif js
+import js.html.*;
+#end
+
+import backend.Paths;
+import backend.Controls;
+import backend.CoolUtil;
+import backend.MusicBeatState;
+import backend.MusicBeatSubstate;
+import backend.CustomFadeTransition;
+import backend.ClientPrefs;
+import backend.Conductor;
+import backend.BaseStage;
+import backend.Difficulty;
+import backend.Mods;
+import backend.Language;
+import mobile.backend.StorageUtil;
+
+import backend.ui.*; //Psych-UI
+
+import objects.Alphabet;
+import objects.BGSprite;
+
+import states.PlayState;
+import states.LoadingState;
+
+#if flxanimate
+import flxanimate.*;
+import flxanimate.PsychFlxAnimate as FlxAnimate;
+#end
+
+//Flixel
+import flixel.sound.FlxSound;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
+import flixel.util.FlxDestroyUtil;
 import flixel.math.FlxMath;
-import flixel.text.FlxText;
-import flixel.util.FlxColor;
-import flixel.tweens.FlxTween;
-import flixel.tweens.FlxEase;
-import flixel.util.FlxTimer;
-import flixel.FlxBasic;
 import flixel.math.FlxPoint;
-import flixel.system.FlxSound;
-#if sys
-import sys.io.*;
-import sys.*;
-#end
-#if DISCORD_ALLOWED
-import funkin.api.Discord;
-import funkin.api.Discord.DiscordClient;
-#end
-#if VIDEOS_ALLOWED
-import hxvlc.flixel.*;
-#end
-import Init;
-#if mobile
-import mobile.StorageUtil;
-#end
-import funkin.Paths;
-import funkin.data.ClientPrefs;
-import funkin.data.Conductor;
-import funkin.utils.CoolUtil;
-import funkin.data.Highscore;
-import funkin.states.*;
-import funkin.objects.BGSprite;
-import funkin.backend.MusicBeatState;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.addons.transition.FlxTransitionableState;
+import shaders.flixel.system.FlxShader;
 
 using StringTools;
 #end
